d1ee4ac2fa484ab78aa11514194d6f294ec923fe
rvk7895 <rkal2009@gmail.com>
2022-03-17 04:21:33 +0530

    collapsable navbar added

--- _next/static/EMP8N86p4syJnQYqBipkr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/302-43742073e3ebe0a4.js","static/css/baa96a3c044b71e9.css","static/chunks/pages/index-e19b1ff9eddc5e17.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
